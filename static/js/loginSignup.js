d91985af74166769d6a1b801a25a8b145072306d
James <138827786+Jameslaw194@users.noreply.github.com>
2025-04-09 13:36:37 +0100

    split up the css and js into separate files

--- static/js/loginSignup.js
+++ static/js/loginSignup.js
@@ -1,35 +1,4 @@
-// Apply dark mode as early as possible to prevent flash of unstyled content
-if (localStorage.getItem("darkMode") === "enabled") {
-	document.documentElement.classList.add("dark");
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-	const body = document.body;
-	const darkModeToggle = document.getElementById("dark-mode-toggle");
-
-	// Dark mode preference
-	if (localStorage.getItem("darkMode") === "enabled") {
-		body.classList.add("dark");
-	}
-
-	function toggleDarkMode() {
-		document.documentElement.classList.toggle('dark');
-				const theme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
-				localStorage.setItem('theme', theme);
-				
-		if (body.classList.contains("dark")) {
-			body.classList.remove("dark");
-			localStorage.setItem("darkMode", "disabled");
-		} else {
-			body.classList.add("dark");
-			localStorage.setItem("darkMode", "enabled");
-		}
-	}
-
-	if (darkModeToggle) {
-		darkModeToggle.checked = body.classList.contains("dark");
-		darkModeToggle.addEventListener("change", toggleDarkMode);
-	}
 
 	// Login/Signup modal handlers
 	window.openModal = function (modalId) {
